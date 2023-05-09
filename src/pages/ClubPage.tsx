c40b328caa27ce2492356ac3d008fabbcd0a5093
eeseohyun <jinlijinyoung@gmail.com>
2023-05-09 17:11:57 +0900

    지역별 카테고리 구분

--- src/pages/ClubPage.tsx
+++ src/pages/ClubPage.tsx
@@ -2,10 +2,13 @@ import { useEffect, useContext, useState } from "react";
 import { listAll, ref, getDownloadURL } from "firebase/storage";
 import {
 	collection,
-	documentId,
 	getDocs,
 	orderBy,
 	query,
+	doc,
+	updateDoc,
+	getDoc,
+	where,
 } from "firebase/firestore";
 import { storage, db } from "../firebase.config";
 import { AuthContext } from "../componenets/AuthContext";
@@ -15,15 +18,26 @@ export default function ClubPage() {
 	const [img, setImg] = useState("");
 	const [posts, setPosts] = useState([]);
 	const [participationBtn, setParticipationBtn] = useState(0);
+	const [category, setCategory] = useState("전체");
 	const { currentUser } = useContext(AuthContext);
 
 	useEffect(() => {
 		const fetchPost = async () => {
 			const board = collection(db, "club");
 			//일정이 빨리 끝나는 순으로 불러옴
-			const querySnapshot = await getDocs(
-				query(board, orderBy("schedule", "asc"))
-			);
+			let querySnapshot;
+			//지역별로 게시물 불러오기
+			if (category === "전체") {
+				querySnapshot = await getDocs(query(board, orderBy("schedule", "asc")));
+			} else {
+				querySnapshot = await getDocs(
+					query(
+						board,
+						where("category", "==", category),
+						orderBy("schedule", "asc")
+					)
+				);
+			}
 			const postData = querySnapshot.docs.map((doc) => ({
 				title: doc.data().title,
 				schedule: doc.data().schedule,
@@ -34,7 +48,7 @@ export default function ClubPage() {
 			}));
 			setPosts(postData);
 		};
-
+		//게시물의 해당하는 이미지 불러오기
 		const fetchImg = async () => {
 			if (currentUser !== null) {
 				const imgRef = ref(storage, `${currentUser.uid}`);
@@ -52,14 +66,73 @@ export default function ClubPage() {
 		};
 		fetchPost();
 		fetchImg();
-	}, []);
+	}, [category]);
 
-	const handleClick = () => {};
+	const handleClick = async (idx) => {
+		const clubRef = doc(db, "club", idx);
+		const clubDoc = await getDoc(clubRef);
+		const participants = clubDoc.data().isParticipationed || [];
+
+		if (!participants.includes(currentUser.uid)) {
+			await updateDoc(clubRef, {
+				isParticipationed: [...participants, currentUser.uid],
+			});
+			setParticipationBtn(participationBtn + 1);
+		}
+	};
 
 	return (
 		<div className="py-8 w-full">
-			<p className="ml-7">지역 | 전체</p>
-
+			<div className="flex justify-center mb-5">
+				<p
+					className="cursor-pointer mr-5 font-medium text-gray-500 hover:text-gray-800"
+					onClick={() => setCategory("전체")}
+				>
+					전체
+				</p>
+				<p
+					className="cursor-pointer mr-5 font-medium text-gray-500 hover:text-gray-800"
+					onClick={() => setCategory("서울")}
+				>
+					서울
+				</p>
+				<p
+					className="cursor-pointer mr-5 font-medium text-gray-500 hover:text-gray-800"
+					onClick={() => setCategory("경기")}
+				>
+					경기
+				</p>
+				<p
+					className="cursor-pointer mr-5 font-medium text-gray-500 hover:text-gray-800"
+					onClick={() => setCategory("강원")}
+				>
+					강원
+				</p>
+				<p
+					className="cursor-pointer mr-5 font-medium text-gray-500 hover:text-gray-800"
+					onClick={() => setCategory("충청")}
+				>
+					충청
+				</p>
+				<p
+					className="cursor-pointer mr-5 font-medium text-gray-500 hover:text-gray-800"
+					onClick={() => setCategory("전라")}
+				>
+					전라
+				</p>
+				<p
+					className="cursor-pointer mr-5 font-medium text-gray-500 hover:text-gray-800"
+					onClick={() => setCategory("경상")}
+				>
+					경상
+				</p>
+				<p
+					className="cursor-pointer mr-5 font-medium text-gray-500 hover:text-gray-800"
+					onClick={() => setCategory("제주")}
+				>
+					제주
+				</p>
+			</div>
 			{posts.map((post) => (
 				<div
 					key={post.idx}
@@ -94,16 +167,16 @@ export default function ClubPage() {
 							<div className="w-1/4 flex flex-col justify-center items-center">
 								<p className="text-xl font-bold">인원</p>
 								<p className="text-xl font-bold">
-									{participationBtn}/{post.participantsNum}
+									{posts.isParticipationed}/{post.participantsNum}
 								</p>
 								<div>
-									{post.participantsNum > participationBtn ? (
-										<button className="mt-1 text-white bg-emerald-500 hover:bg-emerald-600 focus:ring-4 focus:ring-emerald-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800">
-											참여하기
-										</button>
-									) : (
-										<button disabled>모집마감</button>
-									)}
+									<button
+										onClick={() => handleClick(post.idx)}
+										disabled={participationBtn !== 0}
+										className="mt-1 text-white bg-emerald-500 hover:bg-emerald-600 focus:ring-4 focus:ring-emerald-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
+									>
+										{participationBtn === 0 ? "참여하기" : "참여완료"}
+									</button>
 								</div>
 							</div>
 						</div>
